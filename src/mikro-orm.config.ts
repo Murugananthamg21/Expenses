349e5b0a3e5d926915b1935c1e1cc3305b2bb0eb
Murugananthamg21 <g.muruga1706@gmail.com>
2025-01-12 14:30:00 +0530

    set up

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -6,10 +6,10 @@ dotenv.config();
 const config: Options = {
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
-  dbName: process.env.DB_NAME || 'expenses_Tracking',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'rohit45##G',
-  host: process.env.DB_HOST || 'localhost',
+  dbName: process.env.DB_NAME || 'expenses_tvui',
+  user: process.env.DB_USER || 'expenses',
+  password: process.env.DB_PASSWORD || 'alzOvBuxy6lvZ9SrIVnwO09B1acgSD6V',
+  host: process.env.DB_HOST || 'dpg-cu1o7h3qf0us73de9feg-a',
   port: Number(process.env.DB_PORT) || 5432,
   driver: PostgreSqlDriver,
   migrations: {
